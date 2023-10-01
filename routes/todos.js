61202513875bfb4068c920a7f7f804f89c5afc05
amaan16 <amaansubhani16@gmail.com>
2023-10-01 23:47:03 +0530

    Added todos classes

--- routes/todos.js
+++ routes/todos.js
@@ -2,11 +2,32 @@ const express = require("express");
 const router = express.Router();
 // const userController = require('..controllers/todosController');
 
+class TodosOfUser{
+    constructor(userName,userId,todos){
+        this.userName = userName;
+        this.userId = userId;
+        this.todo = todos ? todos.map((todo)=> new Todo (todo.nameOfTodo,todo.display,todo.items)) : [];
+    }
+}
 
+class Todo{
+    constructor(nameOfTodo, display, items){
+        this.nameOfTodo = nameOfTodo;
+        this.display = display;
+        this.items = items.map((itemData)=> new Item(itemData.itemName,itemData.status));
+    }
+}
 
-router.post('' , (req,res)=> {
-    console.log("Users")
-} );
+class Item{
+    constructor(itemName,status){
+        this.itemName = itemName;
+        this.status =status;
+    }
+}
+
+// router.post('' , (req,res)=> {
+//     console.log("Users")
+// } );
 
 
 
